676c2f21fc1a0c99e5977b0d0d4e3741882523e5
dork1 <coder3337@gmail.com>
2024-07-26 18:27:59 +0200

    updated package.json

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const userRoutes = require('./routes/userRoutes.js');
 const userInViews = require('./middleware/allUsersMiddle');
 // const allRoutes = require('./routes/allRoutes');
 const app = express();
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 app.set('trust proxy', 1); // trust first proxy
 
 app.use(cookieSession({
